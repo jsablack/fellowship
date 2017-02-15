0253ee92d7df4adfab7e7e297f9ac36e211f7892
Worn Jerabek Wiltse Architects <WJW@Worns-MacBook-Pro.local>
2017-02-15 15:32:27 -0600

    halfway

--- scripts/fellowship.js
+++ scripts/fellowship.js
@@ -19,33 +19,57 @@ var buddies = [
 var lands = ['The Shire', 'Rivendell', 'Mordor'];
 var body = document.body;
 var section = '<section></section>';
+// var article = '<article></article>';
+
 
 function makeMiddleEarth() {
   // your answers here
+  $('body').append(section);
+  $('section').prop('id', 'middle-earth');
+  for(i = 0; i < lands.length; i++){
+      //add each genres to the list
+      $('#middle-earth').append('<article id="'+lands[i]+'">' + '<h1>' + lands[i] + '</h1>' + '</article>');
+  }
 }
 
 makeMiddleEarth();
 
 function makeHobbits(){
   // your answers here
+  $('article').first().append('<ul>' + '</ul>');
+  for(i = 0; i < hobbits.length; i++){
+    $('ul').append('<li>' + hobbits[i] + '</li>');
+    $('li').prop('class', 'hobbit');
+  }
 }
 
 makeHobbits();
 
 function keepItSecretKeepItSafe(){
   // your answers here
+  var div = $('div').prop('id', 'the-ring').prop('class', 'magic-imbued-jewelry');
+  // var frodo = $('.hobbits[0]');
+  $(".magic-imbued-jewelry").detach().appendTo($('li').first())
+  // $('.hobbits').first().('#magic-imbued-jewelry');
 }
 
 keepItSecretKeepItSafe();
 
 function makeBuddies(){
   // your answers here
+  $('#Rivendell').append('<aside></aside>')
+  $('aside').append('<ul>' + '</ul>')
+  for(i = 0; i < buddies.length; i++){
+    $('aside').append('<li>' + buddies[i] + '</li>');
+  }
+  // $('li').prop('class', 'buddy');
 }
 
 makeBuddies();
 
 function beautifulStranger(){
   // your answers here
+  var strider = $("li:contains('Strider')")
 }
 
 beautifulStranger();
